75b55c5e61f80b718a8afc1f2cbb3a73d4939d16
hubert <hubbertt.k@gmail.com>
2024-10-17 18:43:39 +0200

    feat: create Item model

--- task/apps/prefixes/models.py
+++ task/apps/prefixes/models.py
@@ -6,3 +6,11 @@ class Prefix(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    prefix = models.ForeignKey(Prefix, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
