db9732e175cdf3588786c0d35cdd2e9a47a8933c
hubert <hubbertt.k@gmail.com>
2024-10-18 21:56:27 +0200

    feat: relate Items back to Prefix

--- task/apps/prefixes/models.py
+++ task/apps/prefixes/models.py
@@ -17,7 +17,7 @@ class Prefix(models.Model):
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    prefix = models.ForeignKey(Prefix, on_delete=models.CASCADE)
+    prefix = models.ForeignKey(Prefix, on_delete=models.CASCADE, related_name="items")
 
     def __str__(self) -> str:
         return self.name
